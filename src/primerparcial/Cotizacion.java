a101dde1eae76af2ee6f825326dabb556c6d1820
HectorPalacio <palaciorivas@gmail.com>
2022-06-12 09:52:46 -0500

    Adición de opciones a Cotización

--- src/primerparcial/Cotizacion.java
+++ src/primerparcial/Cotizacion.java
@@ -193,11 +193,157 @@ public class Cotizacion {
     }
     
     public static void desplegarBajos(){
-    
+        FabricaInstrumento fabricaInstrumento = new FabricaInstrumento();
+        //Usamos la fábrica
+        Instrumento instrumento = fabricaInstrumento.getInstrumento("BAJO");
+        Random random = new Random();
+        int rnd = random.nextInt(3 + 1) + 1;
+        
+        Instrumento instRnd;
+        Afinador afinador;
+        if(rnd == 1){
+            instRnd = fabricaInstrumento.getInstrumento("GUITARRA");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+        }else if(rnd == 2){
+            instRnd = fabricaInstrumento.getInstrumento("BAJO");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+        }else{
+            instRnd = fabricaInstrumento.getInstrumento("PIANO");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+    }
+        System.out.println("Total del pedido:");
+        System.out.println("Precio producto: " + instrumento.Precio);
+        System.out.println("Precio producto 2: " + instRnd.Precio);
+        System.out.println("Precio accesorio producto 2: " + afinador.Precio);
+        
+        System.out.println("Total del pedido:");
+        int costoPedido = instrumento.Precio + instRnd.Precio + afinador.Precio;
+        System.out.println(costoPedido);
     }
     
     public static void desplegarPianos(){
+        FabricaInstrumento fabricaInstrumento = new FabricaInstrumento();
+        //Usamos la fábrica
+        Instrumento instrumento = fabricaInstrumento.getInstrumento("PIANO");
+        Random random = new Random();
+        int rnd = random.nextInt(3 + 1) + 1;
         
+        Instrumento instRnd;
+        Afinador afinador;
+        if(rnd == 1){
+            instRnd = fabricaInstrumento.getInstrumento("GUITARRA");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+        }else if(rnd == 2){
+            instRnd = fabricaInstrumento.getInstrumento("BAJO");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+        }else{
+            instRnd = fabricaInstrumento.getInstrumento("PIANO");
+            //Uso de singleton
+            Afinador afinador1 = Afinador.getInstancia();
+            Afinador afinador2 = Afinador.getInstancia();
+            Afinador afinador3 = Afinador.getInstancia();
+            
+            afinador = afinador1;
+            
+            if(afinador1 == afinador2 && afinador2 == afinador3){
+                System.out.println(afinador1.Precio);    
+                System.out.println("Sí Singleton");
+                
+            }else{
+                System.out.println(afinador1.Precio);
+                System.out.println(afinador2.Precio);
+                System.out.println(afinador3.Precio);
+                System.out.println("No Singleton");
+            }
+    }
+        System.out.println("Total del pedido:");
+        System.out.println("Precio producto: " + instrumento.Precio);
+        System.out.println("Precio producto 2: " + instRnd.Precio);
+        System.out.println("Precio accesorio producto 2: " + afinador.Precio);
+        
+        System.out.println("Total del pedido:");
+        int costoPedido = instrumento.Precio + instRnd.Precio + afinador.Precio;
+        System.out.println(costoPedido);
     }
     
     public static void FinalizarPedido(){
